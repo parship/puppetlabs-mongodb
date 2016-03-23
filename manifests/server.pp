3bd71e99d0c7b0ffdb8833782ad21a1726ffa55b
Henning Block <henning.block@parship.com>
2016-03-23 16:41:34 +0100

    turn around dependency between creating admin user (required for enabled authentication) and creating replicasets

--- manifests/server.pp
+++ manifests/server.pp
@@ -167,7 +167,7 @@ class mongodb::server (
 
       # Make sure that the ordering is correct
       if $create_admin {
-        Class['mongodb::replset'] -> Mongodb::Db['admin']
+        Mongodb::Db['admin'] -> Class['mongodb::replset']
       }
 
     }
